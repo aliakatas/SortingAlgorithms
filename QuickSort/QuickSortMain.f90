90042eff917c85f0fadf25be35c6670ddf321ff6
Aristotelis Liakatas <aristotelis.liakatas@gmail.com>
2019-10-15 22:50:21 +0100

    Add files via upload

---
+++ QuickSort/QuickSortMain.f90
@@ -0,0 +1,32 @@
+
+   !> Testing QuickSort Module
+   program quicksort_test
+   use QuickSort_mod
+   use printers_mod
+   
+   implicit none
+ 
+   integer, parameter      :: k = 8
+   integer                 :: A_int(k), A_int_ord(k)
+   real(SP)                :: A_real(k), A_real_ord(k)
+   real(DP)                :: A_dp(k), A_dp_ord(k)
+   integer                 :: i
+ 
+   A_int = [5, 3, 9, 13, 0, 7, 21, 3]
+   A_int_ord = A_int
+   call QuickSort(A_int_ord, k)
+   call showMe(A_int, A_int_ord, k)
+
+   A_real = [9.81_SP, 6.64_SP, -273.15_SP, 42.0_SP, 1.013_SP, 0.5_SP, 28.28_SP, 3.142_SP]
+   A_real_ord = A_real
+   call QuickSort(A_real_ord, k)
+   call showMe(A_real, A_real_ord, k)
+
+   A_dp = [9.81_DP, 6.64_DP, -273.15_DP, 42.0_DP, 1.013_DP, 0.5_DP, 28.28_DP, 3.142_DP]
+   A_dp_ord = A_dp
+   call QuickSort(A_dp_ord, k)
+   call showMe(A_dp, A_dp_ord, k)
+   
+   read(*,*)
+   
+   end program quicksort_test
---
+++ QuickSort/QuickSort_mod.f90
@@ -0,0 +1,183 @@
+   !> Module implementing the QuickSort Algorithm
+   !  for:
+   !      - integer
+   !      - real (single precision)
+   !      - real (double precision)
+   ! An interface is provided to be able to access
+   ! all three procedures accordingly.
+   
+   module quicksort_mod
+   
+   implicit none
+   private
+   
+   integer, parameter         :: SP = kind(1.0)
+   integer, parameter         :: DP = kind(1.d0)
+   
+   public :: QuickSort
+   public :: SP, DP
+   
+   interface QuickSort
+   procedure :: QSort_int, QSort_real, QSort_dp
+   end interface
+   
+   contains
+   
+   ! ==================================================
+   ! INTEGER 
+   recursive subroutine QSort_int(a, na)
+   ! DUMMY ARGUMENTS
+   integer, intent(in)     :: na
+   integer, intent(inout)  :: a(na)
+ 
+   ! LOCAL VARIABLES
+   integer  :: left, right
+   real     :: random
+   integer  :: pivot
+   integer  :: temp
+   integer  :: marker
+   
+   if (na > 1) then
+      
+      call random_number(random)
+      pivot = a(int(random * real(na - 1)) + 1) 
+      left = 0
+      right = na + 1
+      
+      do while (left < right)
+         
+         right = right - 1
+         do while (a(right) > pivot)
+            right = right - 1
+         end do
+         
+         left = left + 1
+         do while (a(left) < pivot)
+            left = left + 1
+         end do
+         
+         if (left < right) then
+            temp = a(left)
+            a(left) = a(right)
+            a(right) = temp
+         end if
+      end do
+      
+      if (left == right) then
+         marker = left + 1
+      else
+         marker = left
+      end if
+      
+      call QSort_int(a(:marker - 1), marker - 1)
+      call QSort_int(a(marker:), na - marker + 1)
+   end if
+   
+   end subroutine QSort_int
+   
+   ! ==================================================
+   ! REAL (SINGLE PRECISION)
+   recursive subroutine QSort_real(a, na)
+   ! DUMMY ARGUMENTS
+   integer, intent(in)     :: na
+   real(SP), intent(inout) :: a(na)
+ 
+   ! LOCAL VARIABLES
+   integer  :: left, right
+   real     :: random
+   real(SP) :: pivot
+   real(SP) :: temp
+   integer  :: marker
+   
+   if (na > 1) then
+      
+      call random_number(random)
+      pivot = a(int(random * real(na - 1)) + 1) 
+      left = 0
+      right = na + 1
+      
+      do while (left < right)
+         
+         right = right - 1
+         do while (a(right) > pivot)
+            right = right - 1
+         end do
+         
+         left = left + 1
+         do while (a(left) < pivot)
+            left = left + 1
+         end do
+         
+         if (left < right) then
+            temp = a(left)
+            a(left) = a(right)
+            a(right) = temp
+         end if
+      end do
+      
+      if (left == right) then
+         marker = left + 1
+      else
+         marker = left
+      end if
+      
+      call QSort_real(a(:marker - 1), marker - 1)
+      call QSort_real(a(marker:), na - marker + 1)
+   end if
+   
+   end subroutine QSort_real
+   
+   ! ==================================================
+   ! REAL (DOUBLE PRECISION)
+   recursive subroutine QSort_dp(a, na)
+   ! DUMMY ARGUMENTS
+   integer, intent(in)     :: na
+   real(DP), intent(inout) :: a(na)
+ 
+   ! LOCAL VARIABLES
+   integer  :: left, right
+   real     :: random
+   real(DP) :: pivot
+   real(DP) :: temp
+   integer  :: marker
+   
+   if (na > 1) then
+      
+      call random_number(random)
+      pivot = a(int(random * real(na - 1)) + 1) 
+      left = 0
+      right = na + 1
+      
+      do while (left < right)
+         
+         right = right - 1
+         do while (a(right) > pivot)
+            right = right - 1
+         end do
+         
+         left = left + 1
+         do while (a(left) < pivot)
+            left = left + 1
+         end do
+         
+         if (left < right) then
+            temp = a(left)
+            a(left) = a(right)
+            a(right) = temp
+         end if
+      end do
+      
+      if (left == right) then
+         marker = left + 1
+      else
+         marker = left
+      end if
+      
+      call QSort_dp(a(:marker - 1), marker - 1)
+      call QSort_dp(a(marker:), na - marker + 1)
+   end if
+   
+   end subroutine QSort_dp
+   
+ 
+end module quicksort_mod
